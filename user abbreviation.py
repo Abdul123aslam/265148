c4211d49a4727cd302ed1e9ed705ca660e4d61ee
abdul123aslam <abdul123aslam@gmail.com>
2021-04-24 13:28:23 +0530

    daily commit

---
+++ user abbreviation.py
@@ -0,0 +1,11 @@
+a="smh"
+if(a=="lol"):
+    print("laughing out loud")
+elif(a=="rofl"):
+    print("rolling on floor laughing")
+elif(a="lmk"):
+    print("let me know")
+elif(a=="smh"):
+    print("shaking my head")
+else:
+    pass
